8b42a92d04d19dc6aceeb755a32cdb4d3503188f
tatendabakozw <tatendabakozw@gmail.com>
2023-09-17 13:46:31 +0200

    feature: added boilerplace for user routes

--- routes/user/user.py
+++ routes/user/user.py
@@ -16,3 +16,27 @@ user_route = Blueprint('user_route',__name__)
 @token_required
 def get_single_user(current_user):
     return jsonify(current_user)
+
+# edit single user
+# patch request
+# /api/user/edit
+@user_route.route("/api/user/single", methods=["PATCH"])
+@token_required
+def edit_single_user(current_user):
+    return jsonify(current_user)
+
+# delete single user
+# delete request
+# /api/user/delete
+@user_route.route("/api/user/delete", methods=["DELETE"])
+@token_required
+def edit_single_user(current_user):
+    return jsonify(current_user)
+
+# get all users and search
+# post request
+# /api/user/all
+@user_route.route("/api/user/delete", methods=["POST"])
+@token_required
+def edit_single_user(current_user):
+    return jsonify(current_user)
